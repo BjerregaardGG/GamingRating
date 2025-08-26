190f50a616213dbfad57e79ba68491154fdcf5bf
BjerregaardGG <oliverbjerregaard1@gmail.com>
2025-08-26 18:22:09 +0200

    to the top button

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ document.querySelectorAll('nav ul li.dropdown .toggle').forEach(btn => {
         this.parentElement.classList.toggle('active');
     });
 });
+
+const topButton = document.getElementById("top_button");
+
+// Vis knappen når man scroller mere end 200px ned
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        topButton.style.opacity = "1";
+        topButton.style.pointerEvents = "auto";
+    } else {
+        topButton.style.opacity = "0";
+        topButton.style.pointerEvents = "none";
+    }
+});
