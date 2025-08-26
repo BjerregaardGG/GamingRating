550c4ae1e7c02596500ae05667cb3a3eaaed3ade
BjerregaardGG <oliverbjerregaard1@gmail.com>
2025-08-26 17:27:55 +0200

    dropdown mobilvenlig

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Klar til fremtidige funktioner, fx klik-tracking
 console.log("Gaming-site loaded!");
+
+document.querySelectorAll('nav ul li.dropdown .toggle').forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault(); // forhindrer standard link
+        this.parentElement.classList.toggle('active');
+    });
+});
